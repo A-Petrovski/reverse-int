3804686960fcc093846fa9ef21c53cc59a5980ef
Alexander Petrovski <APetrovski5069296@gmail.com>
2020-10-05 02:11:21 +0300

    feat: add bad solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    var nString = String(n);
+    var nLength = nString.length;
+    var result = [];
+    for ( var i = 0; i <= nLength; i++){
+    if (nString[nLength-i] !== '-')
+        result[i] = nString[nLength-i];
+    }
+    return result.join('');
 }
+
